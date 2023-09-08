28bd62e273d4a55881d6534b82094d5e6b3726d9
Smartgodswill <ufiagodswill09@gmail.com>
2023-09-08 15:57:49 -0700

    mvvm pathern

---
+++ android/app/src/main/kotlin/com/willest/trymvvm/trymvvm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.willest.trymvvm.trymvvm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/willestAlex/NetworkApp/trymvvm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.willestAlex.NetworkApp.trymvvm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
